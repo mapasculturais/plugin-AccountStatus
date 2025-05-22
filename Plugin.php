ee91b2347f17e976e9d63f441eece934ab5cad8b
israelmelo <israelg.8@gmail.com>
2025-05-22 11:14:46 -0300

    Cria config e register do job no plugin

---
+++ Plugin.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace AccountStatus;
+
+use MapasCulturais\App;
+
+use \AccountStatus\JobTypes\StatusCheck;
+
+class Plugin extends \MapasCulturais\Plugin
+{
+    protected static $instance;
+
+    function __construct(array $config = [])
+    {
+        $config += [
+            'inactive_seal_id' => env('USR_STATUS_INACTIVE_SEAL_ID', 1),
+            'inactive_period' => env('USR_STATUS_INACTIVE_PERIOD', '-1 year'),
+        ];
+
+        parent::__construct($config);
+
+        self::$instance = $this;
+    }
+
+    /**W
+     * @return void
+     */
+    function register()
+    {
+        $app = App::i();
+        
+        $app->registerJobType(new StatusCheck(StatusCheck::SLUG));
+    }
+
+    function _init() {}
+
+    static function getInstance()
+    {
+        return self::$instance;
+    }
+}
