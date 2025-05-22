7c96b8b0ee99e8cfed25ba0071ee36a8dd7e550e
israelmelo <israelg.8@gmail.com>
2025-05-22 11:18:10 -0300

    Criação de db-update para enfileirar o job statusCheck

---
+++ db-updates.php
@@ -0,0 +1,18 @@
+<?php
+
+use MapasCulturais\App;
+use AccountStatus\JobTypes\StatusCheck;
+
+return [
+    'Faz enfileiramento de job responsavel por checar a inatividade do usuario' => function () {
+        $app = App::i();
+        $start_string = date('Y-m-d 00:00:00', strtotime('tomorrow'));
+        $interval_string = '1 day';
+        $iterations = 60*24*365*10;
+
+        $job = $app->enqueueOrReplaceJob(StatusCheck::SLUG, [], $start_string, $interval_string, $iterations);
+        $job->save(true);
+
+        return false;
+    },
+];
