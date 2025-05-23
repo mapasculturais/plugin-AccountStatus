f490740767d706e54cced61d924045707326b561
israelmelo <israelg.8@gmail.com>
2025-05-23 10:29:00 -0300

    Cria db-update para enfileirar o job ProfileUpdateVerification

--- db-updates.php
+++ db-updates.php
@@ -1,6 +1,7 @@
 <?php
 
 use MapasCulturais\App;
+use AccountStatus\JobTypes\ProfileUpdateVerification;
 use AccountStatus\JobTypes\StatusCheck;
 
 return [
@@ -11,6 +12,18 @@ return [
         $iterations = 60*24*365*10;
 
         $job = $app->enqueueOrReplaceJob(StatusCheck::SLUG, [], $start_string, $interval_string, $iterations);
+        $job->save(true);
+
+        return false;
+    },
+
+    'Faz enfileiramento de job responsavel por verificar se o cadastro do usuário está atualizado' => function () {
+        $app = App::i();
+        $start_string = date('Y-m-d 00:00:00', strtotime('tomorrow'));
+        $interval_string = '1 day';
+        $iterations = 60*24*365*10;
+
+        $job = $app->enqueueOrReplaceJob(ProfileUpdateVerification::SLUG, [], $start_string, $interval_string, $iterations);
         $job->save(true);
 
         return false;
